ac9e21f412d9203c566a63bdab389fc83a272e8f
Jason LeBrun <jibbl@google.com>
2024-01-08 18:14:29 +0000

    Move Container Orchestrator Client impl to the SDK

--- oak_containers_hello_world_trusted_app/src/main.rs
+++ oak_containers_hello_world_trusted_app/src/main.rs
@@ -14,8 +14,7 @@
 // limitations under the License.
 
 use anyhow::anyhow;
-use oak_containers_hello_world_trusted_app::orchestrator_client::OrchestratorClient;
-use oak_containers_sdk::InstanceEncryptionKeyHandle;
+use oak_containers_sdk::{InstanceEncryptionKeyHandle, OrchestratorClient};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::net::TcpListener;
 
