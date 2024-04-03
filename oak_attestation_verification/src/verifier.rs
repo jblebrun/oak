a32be89b1f85e19cff901b5913aa98fd4ffb4ec4
Jason LeBrun <jibbl@google.com>
2024-04-03 19:08:47 +0000

    Split verify_measurement_digest into get_expected/compare steps
    This doesn't change anything about the functionality of the verification
    code. It moves us in the direction of being able to returned the
    computed expected values to the client so that they can be provided
    again to avoid re-computation.
    
    b/324837692

--- oak_attestation_verification/src/verifier.rs
+++ oak_attestation_verification/src/verifier.rs
@@ -31,21 +31,21 @@ use oak_dice::cert::{
 };
 use oak_proto_rust::oak::{
     attestation::v1::{
-        attestation_results::Status, binary_reference_value, endorsements,
+        attestation_results::Status, binary_reference_value, endorsements, expected_digests,
         extracted_evidence::EvidenceValues, kernel_binary_reference_value, reference_values,
         root_layer_data::Report, text_reference_value, AmdAttestationReport, AmdSevReferenceValues,
         ApplicationKeys, ApplicationLayerData, ApplicationLayerEndorsements,
         ApplicationLayerReferenceValues, AttestationResults, BinaryReferenceValue, CbData,
         CbEndorsements, CbReferenceValues, ContainerLayerData, ContainerLayerEndorsements,
-        ContainerLayerReferenceValues, Endorsements, Evidence, ExtractedEvidence,
-        FakeAttestationReport, InsecureReferenceValues, IntelTdxAttestationReport,
+        ContainerLayerReferenceValues, Endorsements, Evidence, ExpectedDigests, ExtractedEvidence,
+        FakeAttestationReport, HexDigests, InsecureReferenceValues, IntelTdxAttestationReport,
         IntelTdxReferenceValues, KernelAttachment, KernelBinaryReferenceValue, KernelLayerData,
         KernelLayerEndorsements, KernelLayerReferenceValues, OakContainersData,
         OakContainersEndorsements, OakContainersReferenceValues, OakRestrictedKernelData,
         OakRestrictedKernelEndorsements, OakRestrictedKernelReferenceValues, ReferenceValues,
         RootLayerData, RootLayerEndorsements, RootLayerEvidence, RootLayerReferenceValues,
-        SystemLayerData, SystemLayerEndorsements, SystemLayerReferenceValues, TcbVersion,
-        TeePlatform, TextReferenceValue, TransparentReleaseEndorsement,
+        SkipVerification, SystemLayerData, SystemLayerEndorsements, SystemLayerReferenceValues,
+        TcbVersion, TeePlatform, TextReferenceValue, TransparentReleaseEndorsement,
     },
     HexDigest, RawDigest,
 };
@@ -644,9 +644,22 @@ fn verify_measurement_digest(
     endorsement: Option<&TransparentReleaseEndorsement>,
     reference_value: &BinaryReferenceValue,
 ) -> anyhow::Result<()> {
-    let actual = raw_to_hex_digest(measurement);
+    let expected = get_expected_measurement_digest(now_utc_millis, endorsement, reference_value)?;
+    compare_measurement_digest(measurement, &expected)
+}
+
+// Generate the expected measurement digest values for the provided endorsement
+// and reference_value. The resulting values can be cached by the client to
+// avoid re-computation later.
+fn get_expected_measurement_digest(
+    now_utc_millis: i64,
+    endorsement: Option<&TransparentReleaseEndorsement>,
+    reference_value: &BinaryReferenceValue,
+) -> anyhow::Result<ExpectedDigests> {
     match reference_value.r#type.as_ref() {
-        Some(binary_reference_value::Type::Skip(_)) => Ok(()),
+        Some(binary_reference_value::Type::Skip(_)) => {
+            Ok(ExpectedDigests { r#type: Some(expected_digests::Type::Skip(SkipVerification {})) })
+        }
         Some(binary_reference_value::Type::Endorsement(public_keys)) => {
             let endorsement =
                 endorsement.context("matching endorsement not found for reference value")?;
@@ -658,20 +671,44 @@ fn verify_measurement_digest(
                 &public_keys.endorser_public_key,
                 &public_keys.rekor_public_key,
             )?;
-            let expected = get_digest(&parse_endorsement_statement(&endorsement.endorsement)?)?;
-            verify_hex_digests(&actual, &expected)
+            let digests = {
+                let mut digests = Vec::new();
+                digests.push(get_digest(&parse_endorsement_statement(&endorsement.endorsement)?)?);
+                digests
+            };
+            Ok(ExpectedDigests {
+                r#type: Some(expected_digests::Type::Digests(HexDigests { digests })),
+            })
         }
-        Some(binary_reference_value::Type::Digests(expected_digests)) => {
-            if expected_digests.digests.iter().any(|expected_digest| {
-                let expected = raw_to_hex_digest(expected_digest);
-                verify_hex_digests(&actual, &expected).is_ok()
-            }) {
-                Ok(())
-            } else {
-                Err(anyhow::anyhow!("measurement digest does not match any reference values"))
-            }
-        }
+        Some(binary_reference_value::Type::Digests(expected_digests)) => Ok(ExpectedDigests {
+            r#type: Some(expected_digests::Type::Digests(HexDigests {
+                digests: expected_digests
+                    .digests
+                    .iter()
+                    .map(|raw| raw_to_hex_digest(raw))
+                    .collect(),
+            })),
+        }),
         None => Err(anyhow::anyhow!("empty binary reference value")),
+    }
+}
+
+/// Verifies the measurement digest value against a reference value and
+/// the expected digests calculated from endorsements and reference values.
+fn compare_measurement_digest(
+    measurement: &RawDigest,
+    expected: &ExpectedDigests,
+) -> anyhow::Result<()> {
+    let actual = raw_to_hex_digest(measurement);
+    match expected.r#type.as_ref() {
+        Some(expected_digests::Type::Skip(_)) => Ok(()),
+        Some(expected_digests::Type::Digests(digests)) => digests
+            .digests
+            .iter()
+            .find(|expected| verify_hex_digests(&actual, &expected).is_ok())
+            .map(|_| ())
+            .ok_or(anyhow::anyhow!("measurement digest does not match any reference values")),
+        None => Err(anyhow::anyhow!("empty expected value")),
     }
 }
 
